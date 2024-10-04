702c0b12fddbba9c6a6fedc59db7bd6a44ec1162
lucaspablolabruna <lucaspablolabruna@gmail.com>
2024-10-04 19:16:57 +0000

    Updated NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom"
+import './NavBar.css'	
 
 const NavBar = () => {
   return (
-    <div>
-        <div>
+    <div className="navBar">
+        <Link className="navBar-logo">
             <h1>Jhon Doe</h1>
             <p>Digital Consulting Group</p>
-        </div>
+        </Link>
         <ul>
             <li><Link>Soluciones</Link></li>
             <li><Link>Como trabajamos</Link></li>
             <li><Link>Nosotros</Link></li>
             <li><Link>Servicios</Link></li>
-            <li><Link>Contacto</Link></li>
+            <li><Link className="navBar-contact">Contacto</Link></li>
         </ul>
     </div>
   )
